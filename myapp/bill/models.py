5b0d911e7471cd354df0c3f85d396cc16db153ba
Evgen2325 <github@Evgen2325>
2023-03-01 18:41:29 +0300

    added table view

--- myapp/bill/models.py
+++ myapp/bill/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Bills(models.Model):
     title = models.CharField('Название', max_length=50)
     price = models.IntegerField('Цена', default=0)
-    date = models.DateField('Дата')
+    date = models.DateTimeField('Дата')
 
     def __str__(self):
-        return f'{self.date:%Y-%m-%d}{self.price:->25}р.{self.title:->25}'
+        return f'{self.date:%Y-%m-%d} {self.price} {self.title}'
 
     def get_absolute_url(self):
         return f'/bills/{self.id}'
